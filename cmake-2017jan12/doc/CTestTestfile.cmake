edb5d72ab26d59df9eefb40cf847802c94598821
ethus3h <kolubat@gmail.com>
2018-01-12 18:47:30 -0500

    Update Ember tree

--- cmake-2017jan12/doc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kyan/Ember Library/Ember satellite projects/wreathe-file-manager/doc
-# Build directory: /home/kyan/Ember Library/Ember satellite projects/wreathe-file-manager/cmake-2017jan12/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-2017jan12/src/CMakeFiles/kcm_dolphingeneral.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for kcm_dolphingeneral.
-# This may be replaced when dependencies are built.
