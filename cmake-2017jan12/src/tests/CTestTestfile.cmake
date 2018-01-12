530a7b03dabfd60bf3c3563dae3ce6cf85ab9da0
ethus3h <kolubat@gmail.com>
2018-01-12 18:47:45 -0500

    Update Ember tree

---
+++ cmake-2017jan12/src/tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kyan/Ember Library/Ember satellite projects/wreathe-file-manager/src/tests
+# Build directory: /home/kyan/Ember Library/Ember satellite projects/wreathe-file-manager/cmake-2017jan12/src/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
