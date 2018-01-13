e7eb90f7f5334ac84861bbf89c049d7fa7c8555a
ethus3h <kolubat@gmail.com>
2018-01-12 19:49:05 -0500

    Revert "Update Ember tree"
    This reverts commit edb5d72ab26d59df9eefb40cf847802c94598821.

---
+++ cmake-2017jan12/src/dolphinprivate_autogen/DWW7M6GQBY/moc_dolphinviewcontroller.cpp
@@ -0,0 +1,461 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'dolphinviewcontroller.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.7.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../../../../../../../library/Ember satellite projects/wreathe-file-manager/src/views/dolphinviewcontroller.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#include <QtCore/QList>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'dolphinviewcontroller.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.7.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_DolphinViewController_t {
+    QByteArrayData data[43];
+    char stringdata0[576];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_DolphinViewController_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_DolphinViewController_t qt_meta_stringdata_DolphinViewController = {
+    {
+QT_MOC_LITERAL(0, 0, 21), // "DolphinViewController"
+QT_MOC_LITERAL(1, 22, 18), // "urlChangeRequested"
+QT_MOC_LITERAL(2, 41, 0), // ""
+QT_MOC_LITERAL(3, 42, 4), // "KUrl"
+QT_MOC_LITERAL(4, 47, 3), // "url"
+QT_MOC_LITERAL(5, 51, 18), // "requestContextMenu"
+QT_MOC_LITERAL(6, 70, 3), // "pos"
+QT_MOC_LITERAL(7, 74, 15), // "QList<QAction*>"
+QT_MOC_LITERAL(8, 90, 13), // "customActions"
+QT_MOC_LITERAL(9, 104, 9), // "activated"
+QT_MOC_LITERAL(10, 114, 11), // "urlsDropped"
+QT_MOC_LITERAL(11, 126, 9), // "KFileItem"
+QT_MOC_LITERAL(12, 136, 8), // "destItem"
+QT_MOC_LITERAL(13, 145, 8), // "destPath"
+QT_MOC_LITERAL(14, 154, 11), // "QDropEvent*"
+QT_MOC_LITERAL(15, 166, 5), // "event"
+QT_MOC_LITERAL(16, 172, 14), // "sortingChanged"
+QT_MOC_LITERAL(17, 187, 20), // "DolphinView::Sorting"
+QT_MOC_LITERAL(18, 208, 7), // "sorting"
+QT_MOC_LITERAL(19, 216, 16), // "sortOrderChanged"
+QT_MOC_LITERAL(20, 233, 13), // "Qt::SortOrder"
+QT_MOC_LITERAL(21, 247, 5), // "order"
+QT_MOC_LITERAL(22, 253, 23), // "sortFoldersFirstChanged"
+QT_MOC_LITERAL(23, 277, 12), // "foldersFirst"
+QT_MOC_LITERAL(24, 290, 21), // "additionalInfoChanged"
+QT_MOC_LITERAL(25, 312, 34), // "KFileItemDelegate::Informatio..."
+QT_MOC_LITERAL(26, 347, 4), // "info"
+QT_MOC_LITERAL(27, 352, 13), // "itemTriggered"
+QT_MOC_LITERAL(28, 366, 4), // "item"
+QT_MOC_LITERAL(29, 371, 11), // "itemEntered"
+QT_MOC_LITERAL(30, 383, 12), // "tabRequested"
+QT_MOC_LITERAL(31, 396, 15), // "viewportEntered"
+QT_MOC_LITERAL(32, 412, 11), // "hideToolTip"
+QT_MOC_LITERAL(33, 424, 14), // "cancelPreviews"
+QT_MOC_LITERAL(34, 439, 28), // "requestVersionControlActions"
+QT_MOC_LITERAL(35, 468, 13), // "KFileItemList"
+QT_MOC_LITERAL(36, 482, 5), // "items"
+QT_MOC_LITERAL(37, 488, 11), // "triggerItem"
+QT_MOC_LITERAL(38, 500, 5), // "index"
+QT_MOC_LITERAL(39, 506, 10), // "requestTab"
+QT_MOC_LITERAL(40, 517, 15), // "emitItemEntered"
+QT_MOC_LITERAL(41, 533, 19), // "emitViewportEntered"
+QT_MOC_LITERAL(42, 553, 22) // "updateMouseButtonState"
+
+    },
+    "DolphinViewController\0urlChangeRequested\0"
+    "\0KUrl\0url\0requestContextMenu\0pos\0"
+    "QList<QAction*>\0customActions\0activated\0"
+    "urlsDropped\0KFileItem\0destItem\0destPath\0"
+    "QDropEvent*\0event\0sortingChanged\0"
+    "DolphinView::Sorting\0sorting\0"
+    "sortOrderChanged\0Qt::SortOrder\0order\0"
+    "sortFoldersFirstChanged\0foldersFirst\0"
+    "additionalInfoChanged\0"
+    "KFileItemDelegate::InformationList\0"
+    "info\0itemTriggered\0item\0itemEntered\0"
+    "tabRequested\0viewportEntered\0hideToolTip\0"
+    "cancelPreviews\0requestVersionControlActions\0"
+    "KFileItemList\0items\0triggerItem\0index\0"
+    "requestTab\0emitItemEntered\0"
+    "emitViewportEntered\0updateMouseButtonState"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_DolphinViewController[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      20,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+      15,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  114,    2, 0x06 /* Public */,
+       5,    2,  117,    2, 0x06 /* Public */,
+       9,    0,  122,    2, 0x06 /* Public */,
+      10,    3,  123,    2, 0x06 /* Public */,
+      16,    1,  130,    2, 0x06 /* Public */,
+      19,    1,  133,    2, 0x06 /* Public */,
+      22,    1,  136,    2, 0x06 /* Public */,
+      24,    1,  139,    2, 0x06 /* Public */,
+      27,    1,  142,    2, 0x06 /* Public */,
+      29,    1,  145,    2, 0x06 /* Public */,
+      30,    1,  148,    2, 0x06 /* Public */,
+      31,    0,  151,    2, 0x06 /* Public */,
+      32,    0,  152,    2, 0x06 /* Public */,
+      33,    0,  153,    2, 0x06 /* Public */,
+      34,    1,  154,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+      37,    1,  157,    2, 0x0a /* Public */,
+      39,    1,  160,    2, 0x0a /* Public */,
+      40,    1,  163,    2, 0x0a /* Public */,
+      41,    0,  166,    2, 0x0a /* Public */,
+      42,    0,  167,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void, QMetaType::QPoint, 0x80000000 | 7,    6,    8,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 11, 0x80000000 | 3, 0x80000000 | 14,   12,   13,   15,
+    QMetaType::Void, 0x80000000 | 17,   18,
+    QMetaType::Void, 0x80000000 | 20,   21,
+    QMetaType::Void, QMetaType::Bool,   23,
+    QMetaType::Void, 0x80000000 | 25,   26,
+    QMetaType::Void, 0x80000000 | 11,   28,
+    QMetaType::Void, 0x80000000 | 11,   28,
+    QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 35,   36,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::QModelIndex,   38,
+    QMetaType::Void, QMetaType::QModelIndex,   38,
+    QMetaType::Void, QMetaType::QModelIndex,   38,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void DolphinViewController::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        DolphinViewController *_t = static_cast<DolphinViewController *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->urlChangeRequested((*reinterpret_cast< const KUrl(*)>(_a[1]))); break;
+        case 1: _t->requestContextMenu((*reinterpret_cast< const QPoint(*)>(_a[1])),(*reinterpret_cast< QList<QAction*>(*)>(_a[2]))); break;
+        case 2: _t->activated(); break;
+        case 3: _t->urlsDropped((*reinterpret_cast< const KFileItem(*)>(_a[1])),(*reinterpret_cast< const KUrl(*)>(_a[2])),(*reinterpret_cast< QDropEvent*(*)>(_a[3]))); break;
+        case 4: _t->sortingChanged((*reinterpret_cast< DolphinView::Sorting(*)>(_a[1]))); break;
+        case 5: _t->sortOrderChanged((*reinterpret_cast< Qt::SortOrder(*)>(_a[1]))); break;
+        case 6: _t->sortFoldersFirstChanged((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 7: _t->additionalInfoChanged((*reinterpret_cast< const KFileItemDelegate::InformationList(*)>(_a[1]))); break;
+        case 8: _t->itemTriggered((*reinterpret_cast< const KFileItem(*)>(_a[1]))); break;
+        case 9: _t->itemEntered((*reinterpret_cast< const KFileItem(*)>(_a[1]))); break;
+        case 10: _t->tabRequested((*reinterpret_cast< const KUrl(*)>(_a[1]))); break;
+        case 11: _t->viewportEntered(); break;
+        case 12: _t->hideToolTip(); break;
+        case 13: _t->cancelPreviews(); break;
+        case 14: _t->requestVersionControlActions((*reinterpret_cast< const KFileItemList(*)>(_a[1]))); break;
+        case 15: _t->triggerItem((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 16: _t->requestTab((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 17: _t->emitItemEntered((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 18: _t->emitViewportEntered(); break;
+        case 19: _t->updateMouseButtonState(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 3:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< KFileItem >(); break;
+            }
+            break;
+        case 8:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< KFileItem >(); break;
+            }
+            break;
+        case 9:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< KFileItem >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (DolphinViewController::*_t)(const KUrl & );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::urlChangeRequested)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)(const QPoint & , QList<QAction*> );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::requestContextMenu)) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::activated)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)(const KFileItem & , const KUrl & , QDropEvent * );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::urlsDropped)) {
+                *result = 3;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)(DolphinView::Sorting );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::sortingChanged)) {
+                *result = 4;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)(Qt::SortOrder );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::sortOrderChanged)) {
+                *result = 5;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)(bool );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::sortFoldersFirstChanged)) {
+                *result = 6;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)(const KFileItemDelegate::InformationList & );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::additionalInfoChanged)) {
+                *result = 7;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)(const KFileItem & );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::itemTriggered)) {
+                *result = 8;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)(const KFileItem & );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::itemEntered)) {
+                *result = 9;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)(const KUrl & );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::tabRequested)) {
+                *result = 10;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::viewportEntered)) {
+                *result = 11;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::hideToolTip)) {
+                *result = 12;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::cancelPreviews)) {
+                *result = 13;
+                return;
+            }
+        }
+        {
+            typedef void (DolphinViewController::*_t)(const KFileItemList & );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&DolphinViewController::requestVersionControlActions)) {
+                *result = 14;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject DolphinViewController::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_DolphinViewController.data,
+      qt_meta_data_DolphinViewController,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *DolphinViewController::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *DolphinViewController::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_DolphinViewController.stringdata0))
+        return static_cast<void*>(const_cast< DolphinViewController*>(this));
+    return QObject::qt_metacast(_clname);
+}
+
+int DolphinViewController::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 20)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 20;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 20)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 20;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void DolphinViewController::urlChangeRequested(const KUrl & _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void DolphinViewController::requestContextMenu(const QPoint & _t1, QList<QAction*> _t2)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+
+// SIGNAL 2
+void DolphinViewController::activated()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, Q_NULLPTR);
+}
+
+// SIGNAL 3
+void DolphinViewController::urlsDropped(const KFileItem & _t1, const KUrl & _t2, QDropEvent * _t3)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)), const_cast<void*>(reinterpret_cast<const void*>(&_t3)) };
+    QMetaObject::activate(this, &staticMetaObject, 3, _a);
+}
+
+// SIGNAL 4
+void DolphinViewController::sortingChanged(DolphinView::Sorting _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 4, _a);
+}
+
+// SIGNAL 5
+void DolphinViewController::sortOrderChanged(Qt::SortOrder _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 5, _a);
+}
+
+// SIGNAL 6
+void DolphinViewController::sortFoldersFirstChanged(bool _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 6, _a);
+}
+
+// SIGNAL 7
+void DolphinViewController::additionalInfoChanged(const KFileItemDelegate::InformationList & _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 7, _a);
+}
+
+// SIGNAL 8
+void DolphinViewController::itemTriggered(const KFileItem & _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 8, _a);
+}
+
+// SIGNAL 9
+void DolphinViewController::itemEntered(const KFileItem & _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 9, _a);
+}
+
+// SIGNAL 10
+void DolphinViewController::tabRequested(const KUrl & _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 10, _a);
+}
+
+// SIGNAL 11
+void DolphinViewController::viewportEntered()
+{
+    QMetaObject::activate(this, &staticMetaObject, 11, Q_NULLPTR);
+}
+
+// SIGNAL 12
+void DolphinViewController::hideToolTip()
+{
+    QMetaObject::activate(this, &staticMetaObject, 12, Q_NULLPTR);
+}
+
+// SIGNAL 13
+void DolphinViewController::cancelPreviews()
+{
+    QMetaObject::activate(this, &staticMetaObject, 13, Q_NULLPTR);
+}
+
+// SIGNAL 14
+void DolphinViewController::requestVersionControlActions(const KFileItemList & _t1)
+{
+    void *_a[] = { Q_NULLPTR, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 14, _a);
+}
+QT_END_MOC_NAMESPACE
