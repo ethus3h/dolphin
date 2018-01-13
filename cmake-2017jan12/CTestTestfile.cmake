408da578d239637caa3f672492556603a9f13bd9
ethus3h <kolubat@gmail.com>
2018-01-12 19:49:19 -0500

    Revert "Update Ember tree"
    This reverts commit 1f90c751528e647a3b45ec9dec93f2b65f1c716f.

--- cmake-2017jan12/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kyan/Ember Library/Ember satellite projects/wreathe-file-manager
-# Build directory: /home/kyan/Ember Library/Ember satellite projects/wreathe-file-manager/cmake-2017jan12
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("doc")
